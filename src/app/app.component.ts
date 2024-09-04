c2f510445bbe30892fdbf40142a5fcbf92d715d2
Lucía Ruiz <luciaruiz9722@gmail.com>
2024-09-04 17:45:51 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductsService } from './api/products.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  private readonly productsSvc = inject(ProductsService);
+
+  products$ = this.productsSvc.getAllProducts();
+}
