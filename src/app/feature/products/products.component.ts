8c5b8a32020a95f763f7b911cfaef1bcfb2fbcf0
Lucía Ruiz <luciaruiz9722@gmail.com>
2024-09-18 20:05:26 +0200

    feat: learn to use @if and @for (DAY-5)

--- src/app/feature/products/products.component.ts
+++ src/app/feature/products/products.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProductsService } from '../../api/products.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  productsSvc = inject(ProductsService);
+  products$ = this.productsSvc.getAllProducts();
 
 }
