497daec09c1bf83734c640844f475c57c8cee1c9
Lucía Ruiz <luciaruiz9722@gmail.com>
2024-09-18 21:01:44 +0200

    feat: animations of view-transition-old and view-transition-new (DAY-5)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig,} from '@angular/core';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ErrorResponseInterceptor } from './shared/error-response.interceptor';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()),
+  providers: [provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(withFetch(), withInterceptors([ErrorResponseInterceptor]))]
 };
