1a4a3593479a0934db785ba2c7039c816ffa3b0f
Lucía Ruiz <luciaruiz9722@gmail.com>
2024-10-24 13:30:33 +0200

    feat: add example to solution error with runInInjectionContext (DAY-6)

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, EnvironmentInjector, inject, OnInit, runInInjectionContext } from '@angular/core';
 import { ProductsService } from '../../api/products.service';
 import { AsyncPipe } from '@angular/common';
 import { CardComponent } from '../card/card.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-products',
@@ -10,8 +11,20 @@ import { CardComponent } from '../card/card.component';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   productsSvc = inject(ProductsService);
   products$ = this.productsSvc.getAllProducts();
 
+  productSvc2: any;
+
+  private readonly injector = inject(EnvironmentInjector);
+
+  ngOnInit(): void {
+    runInInjectionContext(this.injector, () => {
+      this.productSvc2 = inject(ProductsService);
+      const result = toSignal(this.products$);
+      console.log("result:", result);
+    });
+
+  }
 }
